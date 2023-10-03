2f110f64f06359070d2bffe1c051f4dcf0f29ee6
Rodion Grigiola <rogrigiola@gmail.com>
2023-10-03 20:33:29 +0200

    initialize quote app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [quote, setQuote] = useState(null);
+  const [status, setStatus] = useState("idle");
+
+  useEffect(() => {
+    const fetchQuotes = async () => {
+      const { data } = await axios.get(
+        "https://jacintodesign.github.io/quotes-api/data/quotes.json"
+      );
+      setQuotes(data);
+    };
+    fetchQuotes();
+  }, []);
+
+  const handleGetQuoteClick = () => {
+    setQuote(quotes[Math.floor(Math.random() * quotes.length)]);
+  };
+
+  const handleStartAppClick = () => {
+    setQuote(quotes[Math.floor(Math.random() * quotes.length)]);
+    setStatus("active");
+  };
+
+  console.log(quote);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      {status === "idle" && <InitialScreen onStartApp={handleStartAppClick} />}
+      {status === "active" && (
+        <Quote quote={quote} onGetQuote={handleGetQuoteClick} />
+      )}
+    </>
+  );
+}
+
+function InitialScreen({ onStartApp }) {
+  return (
+    <div className="initial-screen-container">
+      <h1>Welcome to random quote generator!</h1>
+      <br />
+      <p className="initial-screen-text">Press the button to start!</p>
+      <button className="btn" onClick={onStartApp}>
+        Get Random Quote
+      </button>
     </div>
+  );
+}
+
+function Quote({ quote, onGetQuote }) {
+  const { text, author, tag } = quote;
+  return (
+    <>
+      <div className="quote-container">
+        <div className="tag">#{tag}</div>
+        <div className="quote-text">
+          <i className="fas fa-quote-left"></i>
+          <span className="quote">{text}</span>
+        </div>
+
+        <div className="quote-author">
+          <span className="author">{author ? author : "Author Unknown"}</span>
+        </div>
+
+        <button className="btn" onClick={onGetQuote}>
+          New Quote
+        </button>
+      </div>
+    </>
   );
 }
 
